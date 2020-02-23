9a35d42eb5473a6a46f541348f00bb9894603c0e
Ananya Singh <as2863@njit.edu>
2020-02-23 15:15:30 -0500

    finished getRandomArray function

--- ArrayOfIntegers.java
+++ ArrayOfIntegers.java
@@ -4,15 +4,34 @@ import java.lang.Math;
 public class ArrayOfIntegers{
 
 
-	public int[] getRandomArray(int n){
-		return;
+	public static ArrayList<Integer> getRandomArray(int n){
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		Random random = new Random();
+		int counter = 0;
+		while(n > counter){
+			boolean inArray = false;
+			int number = random.nextInt();
+			for(int i = 0; i<arr.size(); i++){
+				if(arr.get(i) == number){
+					inArray = true;
+				}
+			}
+			if(!inArray){
+				arr.add(number);
+				counter++;
+			}
+		}
+		for(int i = 0; i<arr.size(); i++){
+			System.out.println(arr.get(i));
+		}
+		return arr;
 	}
 
-	public int[] getSortedArray(int n){
-		return;
-	}
+	//public ArrayList<Integer> getSortedArray(int n){
+	//	
+	//}
 
 	public static void main(String[] args){
-
+		System.out.println(getRandomArray(10));
 	}
 }
