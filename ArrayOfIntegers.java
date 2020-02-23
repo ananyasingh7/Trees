75f993f28ec6733057ce6328ece22cbca95963b9
Ananya Singh <as2863@njit.edu>
2020-02-23 14:27:40 -0500

    added functions

---
+++ ArrayOfIntegers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.Math;
+
+public class ArrayOfIntegers{
+
+
+	public int[] getRandomArray(int n){
+		return;
+	}
+
+	public int[] getSortedArray(int n){
+		return;
+	}
+
+	public static void main(String[] args){
+
+	}
+}
