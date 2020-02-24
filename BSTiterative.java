a321f3efc5d8fb5f912af97fe8951e9ee1f59ac9
Ananya Singh <as2863@njit.edu>
2020-02-23 20:47:17 -0500

    getting started with iterative BST

---
+++ BSTiterative.java
@@ -0,0 +1,23 @@
+public BSTinterative{
+
+	private class Node{     //node class
+		Node left, right;   
+		int data;
+	}
+
+	private Node root;
+
+	public void printArray(ArrayList<Integer> arr){  //used to debug
+		for(int i = 0; i<arr.size(); i++){
+			System.out.println(arr.get(i));
+		}
+	}
+
+	
+
+
+
+
+
+
+}
