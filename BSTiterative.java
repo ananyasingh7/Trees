3cf2afc3870b056919b7f8268026fd1086fe7538
Ananya Singh <as2863@njit.edu>
2020-02-26 00:35:17 -0500

    getting started with iterative insertion method

--- BSTiterative.java
+++ BSTiterative.java
@@ -13,10 +13,15 @@ public class BSTiterative{
 								- 2. If it is greater than the root, go to the right subtree
 								- 3. If it is lesser than the root, go to the left subtree
 								- 4. Compare to the root of the subtree and repeat steps 2,3,1
+								- 5. Insert
 
 
 
 	*/
+
+
+	//GROUNDWORK
+
 
 	class Node{                   //Nodes are part of tree
 		Node leftChild;			  //node can have a left child	
@@ -36,6 +41,22 @@ public class BSTiterative{
 	public BSTiterative(){ //constructor that is used to declare a Tree
 		this.root = null; //when Tree is created, root will be automatically set to null
 	}
+
+
+	//FUNCTIONS
+
+	public void insertIter(int item){ //this method inserts into the tree iteratively and takes in the integer as a parameter
+		Node n = new Node(item); //integer is now inserted in node n 
+
+		//Check if root is empty
+		if(root==null){
+			root = n; //left child and right child are automatically set to null
+		}
+
+		//If you reach here, that means root is not empty and tree has nodes inside of it
+	}
+
+
 
 	public static void main(String[] args){
 		BSTiterative Tree = new BSTiterative();
