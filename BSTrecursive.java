774f4ae6aafc1664a00cbaa6600c184c706c4b97
Ananya Singh <as2863@njit.edu>
2020-02-27 00:54:49 -0500

    finished recursive insertion

--- BSTrecursive.java
+++ BSTrecursive.java
@@ -9,7 +9,10 @@ public class BSTrecursive{
 						- right subtree is always greater than the parent
 						- no duplicate nodes
 
-		Recursive Insertion: 
+		Recursive Insertion: 1) compare data item with current node
+							 2) if smaller go left else go right
+							 3) repeat step 2 till you hit null
+							 4) insert! 
 
 
 	*/
@@ -33,9 +36,87 @@ public class BSTrecursive{
 		this.root = null; //when Tree is created, root will be automatically set to null
 	}
 
+	/*
+		PSUEDO CODE FOR INSERT:
+		Insert(tree,item)
+			check empty, insert at root
+		else
+			insertItem(tree,item)
+	*/
+
+	public void insert(int item){
+		Node n = new Node(item); //add item into node
+
+		boolean checkTree = true; //this flag checks the tree to see if its empty or not
+		//Check if root is empty
+		if(root==null){
+			root = n; //left child and right child are automatically set to null
+			checkTree = false; //function is done running
+		}
+
+		if(checkTree == true){
+			//If you reach here, that means root is not empty and tree has nodes inside of it
+			insertRec(root, n); //insert recursively! -- root is being passed in as a parameter because you always want to compare with root first!
+		}
+
+	}
+
+	public void insertRec(Node first, Node node){ //first will be the node we will always compare with, node will be the node we want to insert
+		if(first.item < node.item){ //the node we want to insert belongs in the right subtree
+			if(first.rightChild != null){ //right child node is not null and we need to keep going if node is greater
+				insertRec(first.rightChild, node);
+			}else{ //right child is null and we can set it here! base case 
+				first.rightChild = node; 
+			}
+		}
+
+		if(first.item > node.item){ //the node we want to insert belongs in the left subtree
+			if(first.rightChild != null){ //left child node is not null and we need to keep going if node is greater
+				insertRec(first.leftChild, node);
+			}else{ //left child is null and we can set it here! base case 
+				first.leftChild = node; 
+			}
+		}
+
+	}
+
+	public static void Sort(BSTrecursive tree){ //to sort a binary search tree (ascending), simply do a inOrder Traversal
+		//luckily I created two helper functions that already sort from because of debugging the tree
+		printBST(tree);
+	}
+
+	static void inOrderTraversal(Node node,ArrayList<Integer> arr){
+		if(node != null){
+			inOrderTraversal(node.leftChild, arr);
+			arr.add(node.item);
+			inOrderTraversal(node.rightChild, arr);
+		}
+	}
+
+
+	//need to print out BST
+	static ArrayList<Integer> printBST(BSTrecursive tree){
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		System.out.println("The root of this BST is: " + tree.root.item);
+		inOrderTraversal(tree.root,arr);
+		System.out.print("InOrder Traversal:( ");
+		for(int i = 0; i<arr.size(); i++){
+			System.out.print(arr.get(i) + ",");
+		}
+		System.out.print(" )");
+		return arr;
+	}	
+
 	public static void main(String[] args){
 		BSTrecursive Tree = new BSTrecursive();
-		System.out.println("Test");
+		Tree.insert(30);
+		Tree.insert(20);
+		Tree.insert(40);
+		Tree.insert(10);
+		Tree.insert(25);
+		Tree.insert(22);
+		Tree.insert(50);
+		Sort(Tree);
 	}
 
 	
